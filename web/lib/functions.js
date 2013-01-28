155c19a403fb9e96004b86a0d75e0d0b7a9eb8b7
Pascal Bellard <pascal.bellard@slitaz.org>
2013-01-28 11:46:36 +0100

    lib/functions.js: desc should not be executable (again)

--- web/lib/functions.js
+++ web/lib/functions.js
@@ -1,30 +1,33 @@
 // SliTaz Pizza Javascript functions.
 //
 
-// Check form to avoid empty values and bad email.
-function checkForm() {
-	if(document.forms["pizza"]["flavor"].value == "")
-    {
-        alert("Please enter SliTaz pizza flavor name");
-        document.forms["pizza"]["flavor"].focus();
-        return false;
-    } else {
-		var str = "!@#$%^&*()+=[]\\\';,./{}|\":<>?";
-		for (var i = 0; i < document.forms["pizza"]["flavor"].value.length; i++) {
-		  	if (str.indexOf(document.forms["pizza"]["flavor"].value.charAt(i)) != -1)
-		  	{
-			  	alert ("Invalid Flavor name.\n Please remove special characters.");
-			  	document.forms["pizza"]["desc"].focus();
-			  	return false;
-		  	}
+function charactersOK(field, name) {
+	var str = "`!@#$%^&*()+=[]\\\';,./{}|\":<>?";
+	if(document.forms["pizza"][field].value == "") {
+        	alert("Please enter"+name);
+		document.forms["pizza"][field].focus();
+		return false;
+	}
+	for (var i = 0; i < document.forms["pizza"][field].value.length; i++) {
+		if (str.indexOf(document.forms["pizza"][field].value.charAt(i)) != -1) {
+			alert ("Invalid "+name+".\n Please remove special characters.");
+			document.forms["pizza"][field].focus();
+			return false;
 		}
 	}
-    if(document.forms["pizza"]["desc"].value == "")
-    {
-        alert("Please fill in the flavor description");
-        document.forms["pizza"]["desc"].focus();
-        return false;
-    }
+	return true;
+}
+
+// Check form to avoid empty values and bad email.
+function checkForm() {
+    if (!charactersOK("flavor", "pizza flavor name", 
+    			"`!@#$%^&*()+=[]\\\';,./{}|\":<>?"))
+	return false;
+    if (!charactersOK("desc", "flavor description", 
+    			"`!@#$%^&*()+=[]\\\';,./{}|\":<>?"))
+	return false;
+    if (!charactersOK("mail", "email address", "$`\\"))
+	return false;
 	var x=document.forms["pizza"]["mail"].value;
 	var atpos=x.indexOf("@");
 	var dotpos=x.lastIndexOf(".");
